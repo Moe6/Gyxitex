2f138e8a8e527ae1073e249d4daa64f1d6fdd58a
Addmore <addmore@xbsynergy.co.za>
2018-02-28 13:45:24 +0200

    Send Mail
    Convert xlsm to csv file
    Send attachment as csv

--- MailConsole/MailConsole/Module1.vb
+++ MailConsole/MailConsole/Module1.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Net.Mail
 Imports SpeedyCat
+Imports Microsoft.Office.Interop
 Module Module1
     Private log As New SpeedyCat.Log
 
@@ -21,12 +22,14 @@ Module Module1
             If crpt.ValidateNumberInput(diff) Then
                 'Validate app days/period
                 Dim vld As New SpeedyCat.ValidateUse(Configuration.ConfigurationSettings.AppSettings.Item("vdu")) 'mm-dd-yyyy
-                If vld.UseValidDays(25) Then
+                If vld.UseValidDays(30) Then
                     'get  pass from config file
                     SendMail(_pass)
                 Else
                     log.WriteToTemp("Validaiton failed", "MailConsole")
                 End If
+            Else
+                log.WriteToTemp("Diff Validaiton failed", "MailConsole")
             End If
         Catch ex As Exception
             log.WriteToTemp(ex.Message, "MailConsole")
@@ -41,8 +44,13 @@ Module Module1
         Dim attachment2 As Attachment
         Dim filename1 As String = Configuration.ConfigurationSettings.AppSettings.Item("file1")
         Dim filename2 As String = Configuration.ConfigurationSettings.AppSettings.Item("file2")
-
+        Dim canSend As Boolean
         'Dim path As String = "C:\SYSPRO61\BASE\ESSBASE\"
+
+        'convert to CSV and save 
+        filename1 = ConvertToCSV(filename1)
+        filename2 = ConvertToCSV(filename2)
+
         Try
             SmtpServer.EnableSsl = True
             SmtpServer.Credentials = New Net.NetworkCredential("chlorox.reports@gmail.com", _pwd)
@@ -59,17 +67,29 @@ Module Module1
             mail.Body = "Please see file as at " & Now
 
             'Attachment
-            attachment = New Attachment(filename1) 'file path
-            attachment2 = New Attachment(filename2)
-            If attachment IsNot Nothing Then
-                mail.Attachments.Add(attachment) 'attachment
+            If filename1 IsNot Nothing Then
+                attachment = New Attachment(filename1) 'file path
+                If attachment IsNot Nothing Then
+                    mail.Attachments.Add(attachment) 'attachment
+                    canSend = True
+                End If
             End If
-            If attachment2 IsNot Nothing Then
-                mail.Attachments.Add(attachment2) 'attachment
+            If filename2 IsNot Nothing Then
+                attachment2 = New Attachment(filename2)
+                If attachment2 IsNot Nothing Then
+                    mail.Attachments.Add(attachment2) 'attachment
+                    canSend = True
+                End If
             End If
-            Console.WriteLine("Sending mail...")
-            SmtpServer.Send(mail)
-            Console.WriteLine("Mail Sent.")
+
+            If canSend Then
+                Console.WriteLine("Sending mail...")
+                SmtpServer.Send(mail)
+                Console.WriteLine("Mail Sent.")
+            Else
+                log.WriteToTemp("Files could not be attached", "MailConsole")
+            End If
+
         Catch ex As Exception
             log.WriteToTemp(ex.Message, "MailConsole")
         End Try
@@ -79,6 +99,63 @@ Module Module1
         Process.Start("mailto:mtengwaa@gmail.com?subject=Please see attachment&body=Please  see the attachment")
     End Sub
 
+    Private Function ConvertToCSV(file1 As String) As String
+        Dim ogFile As String = file1
+        Dim newFile As String = ""
+        'Creat file path name for new file
+        If ogFile.EndsWith(".xlsm") Then
+            newFile = Replace(ogFile, ".xlsm", ".csv")
+        End If
+        If ogFile.EndsWith(".xls") Then
+            newFile = Replace(ogFile, ".xls", ".csv")
+        End If
+        If Not DoExcelConversion(ogFile, newFile) Then
+            newFile = Nothing
+        End If
+        Return newFile
+    End Function
 
+    Private Function DoExcelConversion(originalFile As String, newFilePath As String) As Boolean
+        Dim xlApp As New Excel.Application
+        Dim xWorkbook As Excel.Workbook
+        xWorkbook = Nothing
+        Try
+            With xlApp
+                .Visible = False
+                .DisplayAlerts = False
+            End With
+
+            xWorkbook = xlApp.Workbooks.Open(originalFile)
+            'get first worksheet
+            Dim workSheet As Excel.Worksheet = xWorkbook.Worksheets(1)
+
+            If workSheet IsNot Nothing Then
+                'Convert worksheet and save
+                workSheet.SaveAs(newFilePath, Excel.XlFileFormat.xlCSV)
+                If FileIO.FileSystem.FileExists(newFilePath) Then
+                    Return True
+                End If
+            End If
+
+        Catch ex As Exception
+            log.WriteToTemp(ex.Message, "MailConsole")
+        Finally
+            xWorkbook.Close()
+            xlApp.Quit()
+            ReleaseOjects(xWorkbook)
+            ReleaseOjects(xlApp)
+        End Try
+        Return False
+    End Function
+    Private Sub ReleaseOjects(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
 
 End Module
